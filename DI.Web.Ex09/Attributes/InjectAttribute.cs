b16d13dc093f11901420b3394b3f1bebbddaa910
ovation22 <john@1north.com>
2016-07-16 00:00:18 -0400

    Package updates and private set

--- DI.Web.Ex09/Attributes/InjectAttribute.cs
+++ DI.Web.Ex09/Attributes/InjectAttribute.cs
@@ -5,7 +5,7 @@ namespace DI.Web.Ex09.Attributes
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public class InjectAttribute : Attribute
     {
-        public Type BindingType { get; set; }
+        public Type BindingType { get; private set; }
 
         public InjectAttribute(Type bindingType)
         {
